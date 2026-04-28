c592f38f13d3bd87f323cccbb7a938d6c2c8cbc0
krishan kumar sharma <krishan.sharma02@nagarro.com>
2026-04-28 23:15:12 +0530

    feat: update ThreeTileLayout to 1 on top and 2 on bottom

--- packages/hms_room_kit/lib/src/widgets/grid_layouts/three_tile_layout.dart
+++ packages/hms_room_kit/lib/src/widgets/grid_layouts/three_tile_layout.dart
@@ -8,15 +8,13 @@ import 'package:hms_room_kit/src/model/peer_track_node.dart';
 import 'package:hms_room_kit/src/widgets/grid_layouts/listenable_peer_widget.dart';
 
 ///This widget renders three tiles on a page
-///The three tiles are rendered in a 3x1 grid
+///The three tiles are rendered in a 2x2 grid with one tile on top and two on bottom
 ///The tiles look like this
-/// ┌─────┐
-/// │  0  │
-/// ├─────┤
-/// │  1  │
-/// ├─────┤
-/// │  2  │
-/// └─────┘
+/// ┌───────┐
+/// │   0   │
+/// ├───┬───┤
+/// │ 1 │ 2 │
+/// └───┴───┘
 class ThreeTileLayout extends StatelessWidget {
   final int startIndex;
   final List<PeerTrackNode> peerTracks;
@@ -28,10 +26,9 @@ class ThreeTileLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ///Here we render three rows with one tile in each row
+    ///Here we render two rows
     ///The first row contains the tile with index [startIndex]
-    ///The second row contains the tile with index [startIndex+1]
-    ///The third row contains the tile with index [startIndex+2]
+    ///The second row contains two tiles with indices [startIndex+1] and [startIndex+2]
     return Column(
       children: [
         Expanded(
@@ -42,16 +39,22 @@ class ThreeTileLayout extends StatelessWidget {
         ),
         const SizedBox(height: 2),
         Expanded(
-          child: ListenablePeerWidget(
-            index: startIndex + 1,
-            peerTracks: peerTracks,
-          ),
-        ),
-        const SizedBox(height: 2),
-        Expanded(
-          child: ListenablePeerWidget(
-            index: startIndex + 2,
-            peerTracks: peerTracks,
+          child: Row(
+            children: [
+              Expanded(
+                child: ListenablePeerWidget(
+                  index: startIndex + 1,
+                  peerTracks: peerTracks,
+                ),
+              ),
+              const SizedBox(width: 2),
+              Expanded(
+                child: ListenablePeerWidget(
+                  index: startIndex + 2,
+                  peerTracks: peerTracks,
+                ),
+              ),
+            ],
           ),
         ),
       ],
